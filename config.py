69486ae5f55a5fe3d19265d29a609b4d17819411
REAGAN2020 <reaganteflon@gmail.com>
2020-11-04 21:39:09 +0300

    added some functionlity on config

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+
+
+class Config:
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/rblog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
+    QUOTE_API_KEY = os.environ.get('QUOTE_API_KEY')
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    CSRF_ENABLED = True
+    
+
+
+
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/rblog'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
